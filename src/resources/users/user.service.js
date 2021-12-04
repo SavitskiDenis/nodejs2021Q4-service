9aa7fa2b5a191b9507258cc3041cd99064c27f25
SavitskiDenis <random.ssd@yandex.by>
2021-12-04 17:22:25 +0300

    feat: implement users service

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,17 @@
 const usersRepo = require('./user.memory.repository');
+const tasksService = require('../tasks/task.service');
 
 const getAll = () => usersRepo.getAll();
 
-module.exports = { getAll };
+const getById = (id) => usersRepo.getById(id);
+
+const addUser = (payload) => usersRepo.add(payload);
+
+const updateUser = (id, payload) => usersRepo.update(id, payload);
+
+const deleteUser = (id) => {
+    tasksService.deleteTasksFromUser(id);
+    return usersRepo._delete(id);
+}
+
+module.exports = { getAll, getById, addUser, updateUser, deleteUser };
