99bb7cf8c369ac1b0e0ef516c33128f80e15a3a7
SavitskiDenis <random.ssd@yandex.by>
2021-12-04 17:23:18 +0300

    feat: impelement users router

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,11 +1,131 @@
-const router = require('express').Router();
-const User = require('./user.model');
-const usersService = require('./user.service');
+const usersController = require('./user.controller');
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
-});
+const responseUserSchema = {
+  type: 'object',
+  properties: {
+    id: {
+      type: 'string'
+    },
+    name: {
+      type: 'string'
+    },
+    login: {
+      type: 'string'
+    }
+  }
+};
+
+const responseParams = {
+  userId: {
+    type: 'string',
+    format: 'uuid'
+  }
+};
+
+const requestUserSchema = {
+  type: 'object',
+  properties: {
+    name: {
+      type: 'string'
+    },
+    login: {
+      type: 'string'
+    },
+    password: {
+      type: 'string'
+    }
+  },
+  required: ['name', 'login', 'password']
+};
+
+const getAllUsersOpts = {
+  schema: {
+    response: {
+      200: {
+        type: 'array',
+        items: responseUserSchema
+      }
+    }
+  },
+  handler: usersController.getAllUsers
+};
+
+const getUserByIdOpts = {
+  schema: {
+    params: responseParams,
+    response: {
+      200: responseUserSchema,
+      404: {
+        type: 'string'
+      },
+      400: {
+        type: 'string'
+      }
+    }
+  },
+  handler: usersController.getUserById
+};
+
+const addUserOpts = {
+  schema: {
+    body: requestUserSchema,
+    response: {
+      201: responseUserSchema,
+      404: {
+        type: 'string'
+      },
+      400: {
+        type: 'string'
+      }
+    }
+  },
+  handler: usersController.addUser
+};
+
+const updateUserOpts = {
+  schema: {
+    params: responseParams,
+    body: requestUserSchema,
+    response: {
+      200: responseUserSchema,
+      404: {
+        type: 'string'
+      },
+      400: {
+        type: 'string'
+      }
+    }
+  },
+  handler: usersController.updateUser
+};
+
+const deleteUserOpts = {
+  schema: {
+    params: responseParams,
+    response: {
+      404: {
+        type: 'string'
+      },
+      400: {
+        type: 'string'
+      }
+    }
+  },
+  handler: usersController.deleteUser
+};
+
+const router = (fastify, _, done) => {
+  fastify.get('/users', getAllUsersOpts);
+
+  fastify.get('/users/:userId', getUserByIdOpts);
+
+  fastify.post('/users', addUserOpts);
+
+  fastify.put('/users/:userId', updateUserOpts);
+
+  fastify.delete('/users/:userId', deleteUserOpts);
+
+  done();
+};
 
 module.exports = router;
