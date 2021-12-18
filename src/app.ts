b0105a53a000bc65436bd2c8f94ba1669d35586b
SavitskiDenis <random.ssd@yandex.by>
2021-12-18 15:55:01 +0300

    feat: rework app for ts

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import { FastifyInstance, fastify, FastifyRegisterOptions, FastifyPluginOptions } from 'fastify';
+import { Server, IncomingMessage, ServerResponse } from "http";
+import path from 'path';
+import swagger from 'fastify-swagger';
+import userRouter from './resources/users/user.router';
+import taskRouter from './resources/tasks/task.router';
+import boardRouter from './resources/boards/board.router';
+
+const app: FastifyInstance<Server, IncomingMessage,ServerResponse> = fastify({ logger: true });
+
+const swaggerOptions: FastifyRegisterOptions<FastifyPluginOptions> = {
+  exposeRoute: true,
+  routePrefix: '/doc',
+  mode: 'static',
+  specification: {
+    path: path.join(__dirname, '../doc/api.yaml')
+  },
+};
+
+app.register(swagger, swaggerOptions);
+
+app.register(boardRouter);
+app.register(taskRouter);
+app.register(userRouter);
+
+export default app;
