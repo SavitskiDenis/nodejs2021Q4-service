0dbb266508da27feec6ff02feed0e430aca137d4
SavitskiDenis <random.ssd@yandex.by>
2022-02-06 12:41:00 +0300

    feat: add auth guart and multer storage

--- src/resources/files/files.controller.ts
+++ src/resources/files/files.controller.ts
@@ -1,16 +1,28 @@
-import { Controller, Get, HttpCode, NotFoundException, Param, Post, StreamableFile, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, HttpCode, NotFoundException, Param, Post, StreamableFile, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { createReadStream, existsSync } from 'fs';
+import { diskStorage } from 'multer';
 import { join } from 'path';
+import config from '../../common/config';
+import { AuthGuard } from '../../common/authentification';
 import HTTP_CODES from '../../common/http_codes';
 
 @Controller('file')
-@UseInterceptors(FileInterceptor('file', { dest: 'uploads/' }))
+@UseGuards(AuthGuard)
+@UseInterceptors(FileInterceptor('file', {
+  storage: diskStorage({
+    destination: config.UPLOADS_PATH,
+    filename: (_, file, cb) => {
+        const splitedOrigin = file.originalname.split('.')
+        cb(null, `${Date.now()}.${splitedOrigin[splitedOrigin.length - 1]}`)
+    }
+  })
+}))
 export class FilesController {
   @Post('/')
   @HttpCode(HTTP_CODES.CODE_OK)
   uploadFile(@UploadedFile() file: Express.Multer.File): string {
-    return `File save with name ${file.filename}`;
+    return `File saved with name ${file.filename}`;
   }
 
   @Get('/:fileName')
