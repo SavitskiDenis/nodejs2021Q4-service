bf29e92696a8672e5f9c8d278addeb49b11a336d
SavitskiDenis <random.ssd@yandex.by>
2022-02-06 11:13:35 +0300

    feat: set logger like private instance

--- src/middlewares/RequestMidlware.ts
+++ src/middlewares/RequestMidlware.ts
@@ -1,23 +1,24 @@
-import { NestMiddleware } from "@nestjs/common";
+import { Logger, NestMiddleware } from "@nestjs/common";
 import { Request, Response } from "express";
-import logger from "../common/logger";
 
 export class RequestMiddleware implements  NestMiddleware {
+  private readonly logger = new Logger();
+
   use(req: Request, res: Response, next: () => void) {
     const requestMsg = `method: ${req.method}, url: ${req.url}, query: ${JSON.stringify(req.query)}, body: ${JSON.stringify(req.body)}`;
-    logger.log(requestMsg);
+    this.logger.log(requestMsg);
 
     res.on('close', () => {
       const { statusCode } = res;
       if (statusCode < 400) {
-        logger.log(`Response code ${statusCode}`);
+        this.logger.log(`Response code ${statusCode}`);
         return;
       } if (statusCode >= 400 && statusCode < 500) {
-        logger.warn(`Response code ${statusCode}`);
+        this.logger.warn(`Response code ${statusCode}`);
         return;
       }
 
-      logger.error(`Response code ${statusCode}, request - ${requestMsg}`);
+      this.logger.error(`Response code ${statusCode}, request - ${requestMsg}`);
     });
 
     next();
